8b6e433ada046d6d7b1041d117496e3cb66a8f80
cheeze2000 <me@cheeze2000.net>
2021-12-02 23:17:54 +0800

    fix: add router history base url

--- src/router.js
+++ src/router.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import routes from "virtual:generated-pages";
 
 const router = createRouter({
-	history: createWebHistory(),
+	history: createWebHistory(import.meta.env.BASE_URL),
 	routes
 });
 
