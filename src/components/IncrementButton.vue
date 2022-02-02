1994bf5cf06ca6179123ec5ea425b81837397b9a
cheeze2000 <me@cheeze2000.net>
2022-02-02 16:19:27 +0800

    feat: a better example to showcase vue

---
+++ src/components/IncrementButton.vue
@@ -0,0 +1,5 @@
+<template>
+	<button class="block p-3 mx-auto bg-gray-300">
+		Increment!
+	</button>
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,24 @@
+<script setup>
+import { reactive } from "vue";
+
+import IncrementButton from "~/components/IncrementButton.vue";
+
+const data = reactive({
+	number: 0
+});
+
+function incrementNumber() {
+	data.number++;
+}
+</script>
+
 <template>
-	<h1 class="text-3xl text-pink-400">
-		Hello, World!
-	</h1>
+	<div class="flex justify-center items-center h-screen">
+		<div class="text-3xl text-center">
+			<p class="my-6">
+				Value: {{ data.number }}
+			</p>
+			<IncrementButton @click="incrementNumber" />
+		</div>
+	</div>
 </template>
